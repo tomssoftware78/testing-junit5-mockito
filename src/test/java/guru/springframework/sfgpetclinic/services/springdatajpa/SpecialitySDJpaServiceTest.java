9ca53d57bc95a50004465b9d2ff7f7f1a90ec3f9
tomvde <D-8110tr>
2020-01-13 20:13:42 +0100

    assert respository delete calls

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -4,9 +4,12 @@ import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -20,10 +23,15 @@ class SpecialitySDJpaServiceTest {
     @Test
     void deleteById() {
         service.deleteById(1l);
+
+        verify(specialtyRepository, times(1)).deleteById(1l);
     }
 
     @Test
     void testDelete() {
-        service.delete(new Speciality());
+        Speciality speciality = new Speciality();
+        service.delete(speciality);
+
+        BDDMockito.then(specialtyRepository).should(times(1)).delete(speciality);
     }
 }
